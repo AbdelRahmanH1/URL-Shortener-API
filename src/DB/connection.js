52640e30b8b92ae5bcc9bd8028d2466da1392f87
Abdel Rahman <hossamabdelrahman62@gmail.com>
2024-11-05 01:34:13 +0200

    fix connection error

--- src/DB/connection.js
+++ src/DB/connection.js
@@ -2,8 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DATABASEURL);
-
+    await mongoose.connect(process.env.DATABASEURL, { dbName: 'shorter' });
     console.log(`DataBase connect successfully`);
   } catch (error) {
     console.error(`Database connection error: ${error.message}`);
