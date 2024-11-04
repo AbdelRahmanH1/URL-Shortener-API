405168f4dc07c428f20e3d509cd8fc9f65230af2
Abdel Rahman <hossamabdelrahman62@gmail.com>
2024-11-04 21:24:41 +0200

    Add the connection

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { connectDB } from './DB/connection.js';
+
+import { errorResponse } from './utils/errorResponse.utils.js';
 
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 const app = express();
 const PORT = process.env.PORT;
 
-console.log(process.env.NODE_ENV);
+await connectDB();
+app.get('/', (req, res, next) => {
+  return res.json('Hello');
+});
+
+app.all('*', (req, res, next) => {
+  const err = new Error('Content not found', { cause: 404 });
+  next(err);
+});
+app.use(errorResponse);
 
 app
   .listen(PORT, () => {
