392aa24f53f06626b04498d42081b5674b01d86b
Abdel Rahman <hossamabdelrahman62@gmail.com>
2024-11-04 22:56:52 +0200

    Add middleware expressJson & add router

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './DB/connection.js';
-
+import urlRouter from './routers/link.router.js';
 import { errorResponse } from './utils/errorResponse.utils.js';
 
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 const app = express();
 const PORT = process.env.PORT;
-
+app.use(express.json());
 await connectDB();
 app.get('/', (req, res, next) => {
   return res.json('Hello');
 });
-
+app.use(urlRouter);
 app.all('*', (req, res, next) => {
   const err = new Error('Content not found', { cause: 404 });
   next(err);
