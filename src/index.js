590ccc50d7de8c3a57d891dec50f78bfe6438a36
Abdel Rahman <hossamabdelrahman62@gmail.com>
2024-11-07 01:45:52 +0200

    Update index and server

--- src/index.js
+++ src/index.js
@@ -1,7 +1,30 @@
+import { connectDB } from './DB/connection.js';
+import urlRouter from './routers/link.router.js';
+import { errorResponse } from './utils/errorResponse.utils.js';
+import dotenv from 'dotenv';
 import express from 'express';
+dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
+const app = express();
+app.use(express.json());
+const PORT = process.env.PORT;
 
-export const startServer = () => {
-  const app = express();
-  app.use(express.json());
-  return app;
-};
+await connectDB();
+app.get('/', (req, res, next) => {
+  return res.send('Hello');
+});
+
+app.use(urlRouter);
+app.all('*', (req, res, next) => {
+  const err = new Error('Content not found', { cause: 404 });
+  next(err);
+});
+app.use(errorResponse);
+
+app.listen(PORT, () => {
+  console.log(`Server Start at port ${PORT}`);
+});
+
+app.on('error', () => {
+  console.log(`Server Error`);
+});
+export const server = app;
